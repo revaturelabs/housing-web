b7ff14ef59f277b52c6c430884c648f84e5825f6
Paul Santiago <paulsantiago282@gmail.com>
2017-01-25 10:59:39 -0500

    changes

--- Angular/scripts/routes.js
+++ Angular/scripts/routes.js
@@ -1,48 +1,8 @@
-angular.module('congo', ['ui.router'])
+angular.module('HousingApp', ['ui.router'])
 .config(function ($stateProvider) {
     $stateProvider
     .state('home', {
         url: "",
         templateUrl: "views/home.html",
-    })
-    .state('products', {
-        url: "/products",
-        templateUrl: "views/productList.html",
-    })
-    .state('product', {
-        url: "/products/{ProductID}",
-        controller: "productCtrl",
-        templateUrl: "views/product.html",
-    })
-    .state('login', {
-        url: "/login",
-        templateUrl: "views/login.html",
-    })
-    .state('createAccount', {
-        url: "/createaccount",
-        templateUrl: "views/createAccount.html",
-    })
-    .state('cart', {
-        url: "/cart",
-        controller: "cartCtrl",
-        templateUrl: "views/Cart.html",
-    })
-    .state('checkout',{
-        url:"/checkout",
-        controller: "stripeCtrl",
-        templateUrl: "views/checkout.html",
-    })
-    .state('orders', {
-        url: "/orders",
-        controller: "ordersCtrl",
-        templateUrl: "views/orders.html"
-    })
-    .state('order', {
-        url: "/order/{OrderID}",
-        params: {
-            message: null
-        },
-        controller: "orderCtrl",
-        templateUrl: "views/Cart.html"
     });
 });
